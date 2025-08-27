cd3e042140ca107b8957fdf5723999469c74b253
GAUTAM KUMAR SINGH <dee70a93-c6d3-4edd-a7c2-4a6659a185e7>
2025-08-27 05:50:22 +0000

    UserMgt_01 Commit

--- sa-user-mgt/webapp/controller/Main.controller.js
+++ sa-user-mgt/webapp/controller/Main.controller.js
@@ -9,18 +9,48 @@ sap.ui.define([
 
             var oModel = new JSONModel();
             this.getView().setModel(oModel, "users");
+            var vURL = "/scim-api/Users";
+            var sURL = this._getExternalServiceRuntimeBaseURL() + vURL;
+
+
             $.ajax({
-                url: "/scim-api/Users",
+                url: sURL,
                 method: "GET",
+                dataType: "json",
+                contentType: "application/json",
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("Accept", "application/json");
+                },
                 success: function (data) {
                     console.log("Fetched users successfully");
-                    oModel.setData(data.Resources || []);
+                    console.log(data);
+                    oModel.setData(data.resources || []);
                 },
                 error: function (err) {
+                    console.log("Failed-", err.status + ": " + err.statusText);
+                    alert("An error occurred while fetching users.");
                     console.error("Error fetching users", err);
                 }
             })
 
-        }
+        },
+
+        _getExternalServiceRuntimeBaseURL: function () {
+            var oComponent = sap.ui.core.Component.getOwnerComponentFor(this.getView());
+
+            if (oComponent) {
+                var sAppId = oComponent.getManifestEntry("/sap.app/id");
+                console.log("sAppId-" + sAppId);
+                var sAppPath = sAppId.replaceAll(".", "/");
+                var sAppModulePath = jQuery.sap.getModulePath(sAppPath);
+                console.log("sAppModulePath-" + sAppModulePath);
+
+                return sAppModulePath;
+            }
+            else {
+                console.error("Component could not be found.");
+                return "";
+            }
+        },
     });
 });
